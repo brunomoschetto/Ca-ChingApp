a62754c4a68be16c8d8699e9627179c9e53476d9
Bruno Moschetto <bruno.moschetto@gmail.com>
2025-03-24 12:32:18 +0100

    Created ProductsController#index action with proper View and tests both manual and through rspec gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'products/index'
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :products, only: [:index]
---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe ProductsController, type: :controller do
+  describe "GET #index" do
+    it "assigns @products" do
+      product = Product.create(code: "GR1", name: "Green Tea", price: 3.11)
+      get :index
+      expect(assigns(:products)).to eq([product])
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+end
