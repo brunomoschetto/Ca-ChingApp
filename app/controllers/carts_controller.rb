69013262e9264433d686fc91ec444839b63ef431
Bruno Moschetto <bruno.moschetto@gmail.com>
2025-03-25 11:28:52 +0100

    delete button implemented

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,13 +8,29 @@ class CartsController < ApplicationController
     product = Product.find(params[:product_id])
     @cart = current_cart
     cart_item = @cart.cart_items.find_by(product_id: product.id)
+
     if cart_item
       cart_item.quantity += 1
       cart_item.save
     else
       @cart.cart_items.create(product: product, quantity: 1)
     end
+    
     redirect_to cart_path, notice: "#{product.name} added to cart!"
+  end
+
+  def remove_item
+    @cart = current_cart
+    cart_item = @cart.cart_items.find(params[:cart_item_id])
+
+    if cart_item.quantity > 1
+      cart_item.quantity -= 1
+      cart_item.save
+    else
+      cart_item.destroy
+    end
+
+    redirect_to cart_path, notice: "Item removed from cart"
   end
 
   private
