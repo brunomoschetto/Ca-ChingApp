184edf11ac62de41b611bc077af6399dacffd640
Bruno Moschetto <bruno.moschetto@gmail.com>
2025-03-25 17:56:19 +0100

    set the products#index view as 'home-page'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
+  root 'products#index'
 
   resources :products, only: [:index]
+
   resource :cart, only: [:show] do
     post :add_product
     delete :remove_item
   end
+  
 end
