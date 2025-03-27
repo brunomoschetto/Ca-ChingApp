7f9c90b305a2789d12c17e4e5564f552b333b7dd
Bruno Moschetto <bruno.moschetto@gmail.com>
2025-03-27 17:55:05 +0100

    private methods refactor

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -26,12 +26,4 @@ class CartsController < ApplicationController
 
     redirect_to cart_path, notice: "Item removed from cart"
   end
-
-  private
-
-  def current_cart
-    Cart.find_or_create_by(id: session[:cart_id]).tap do |cart|
-      session[:cart_id] = cart.id
-    end
-  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,14 +3,4 @@ class ProductsController < ApplicationController
     @products = Product.all
     @cart = current_cart
   end
-
-  private
-
-  def current_cart
-    Cart.find(session[:cart_id])
-  rescue ActiveRecord::RecordNotFound
-    cart = Cart.create
-    session[:cart_id] = cart.id
-    cart
-  end
 end
