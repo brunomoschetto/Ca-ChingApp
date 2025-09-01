1d37a1040123e4bb608d8f4cddd7d53eef37d5ec
Bruno Moschetto <bruno.moschetto@gmail.com>
2025-09-01 15:09:30 +0200

    test(controller): update redirects and notices to match new HTML fallbacks

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe CartsController, type: :controller do
       it "redirects to the cart path with a success notice" do
         post :add_product, params: { product_id: product.id }
         expect(response).to redirect_to(products_path)
-        expect(flash[:notice]).to eq("#{product.name} added to cart!")
+        expect(flash[:notice]).to eq("Added to cart")
       end
     end
 
@@ -81,7 +81,7 @@ RSpec.describe CartsController, type: :controller do
     it "redirects to the cart path with a success notice" do
       delete :remove_item, params: { cart_item_id: cart_item.id }
       expect(response).to redirect_to(products_path)
-      expect(flash[:notice]).to eq("Item removed from cart")
+      expect(flash[:notice]).to eq("Item removed")
     end
   end
 
