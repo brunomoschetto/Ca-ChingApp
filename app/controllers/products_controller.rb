16e7ed6fec9825a08533e61aef48d55e23a0709f
Bruno Moschetto <bruno.moschetto@gmail.com>
2025-09-04 13:02:14 +0200

    feat(api): expose /products.json with basic fields and promo info

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,24 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    @cart = current_cart
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: @products.map { |p|
+          rule = DiscountRule.find_by(product_code: p.code)
+          {
+            id: p.id,
+            code: p.code,
+            name: p.name,
+            price: p.price,
+            promo: rule&.rule_type,
+            promo_value: rule&.value
+          }
+        }
+      end
+    end
   end
 
   private
